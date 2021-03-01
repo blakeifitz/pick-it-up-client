b8e0735542b140d8eac626485af51d8b6d3124ab
blakeifitz <blakeifitz@gmail.com>
2021-02-28 19:18:00 -0500

    build for vercel production

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../components/App/App';
+import App from '../components/app/app';
 
 describe('<App />', () => {
   it('Renders without crashing', () => {
